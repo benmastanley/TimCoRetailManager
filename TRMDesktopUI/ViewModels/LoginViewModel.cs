6a10087e2e015307e1b60576c4ffcc899992852a
benmastanley <66832226+benmastanley@users.noreply.github.com>
2021-01-04 17:10:32 +0000

    Login Form Error Handling
    Created error handling on the wpf login form - checks database via API to see if user exists with matching password - if not, displays error message on screen.

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -38,6 +38,32 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+        public bool IsErrorVisible
+        {
+            get {
+                bool output = false;
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+
+                }
+                return output; 
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { 
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
+
         public bool CanLogIn
         {
             get
@@ -55,12 +81,13 @@ namespace TRMDesktopUI.ViewModels
         {
             try
             {
-            var result = await _apiHelper.Authenticate(UserName, Password);
+                ErrorMessage = "";
+                var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch(Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
-    }
+    } 
 }
