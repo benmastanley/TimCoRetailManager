d8c996005f82607d185ce8c5ed5d7be8ae37f486
benmastanley <66832226+benmastanley@users.noreply.github.com>
2021-01-04 20:06:19 +0000

    Getting User Data

---
+++ TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128)
+AS
+begin
+	set nocount on; 
+
+	SELECT Id, FirstName, LastName, EmailAddress
+	from [dbo].[User]
+	where Id =  @Id;
+end 
